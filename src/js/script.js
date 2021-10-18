2f26ff3a66559cc81ebd5ed8da87cb6b98144f7c
MrRobot-p <youwaytosuccess@gmail.com>
2021-10-18 10:14:14 +0300

    Fixed issues

--- src/js/script.js
+++ src/js/script.js
@@ -11,8 +11,8 @@ const AnswerOpenedClass = 'answers--answer-opened';
 // МОДАЛЬНОЕ ОКНО
 
 var write_btn = document.querySelector(".contacts-button");
-var write_popap = document.querySelector(".modal-write");
-var write_close = write_popap.querySelector(".modal-close");
+var write_popap = document.querySelector(".modal");
+var write_close = write_popap.querySelector(".modal__close-btn");
 var overlay = document.querySelector(".overlay-modal");
 var form = write_popap.querySelector("form");
 var user_name = form.querySelector("input[type=text]");
@@ -29,7 +29,7 @@ try {
 
 write_btn.addEventListener("click", function(evt) {
     evt.preventDefault();
-    write_popap.classList.add("modal-show");
+    write_popap.classList.add("modal--show");
     overlay.classList.add('active');
     if (storage) {
         user_name.value = storage;
@@ -52,7 +52,7 @@ form.addEventListener("submit", function(evt) {
 
 write_close.addEventListener("click", function(evt) {
     evt.preventDefault();
-    write_popap.classList.remove("modal-show");
+    write_popap.classList.remove("modal--show");
     write_popap.classList.remove("modal-error");
     overlay.classList.remove('active');
 });
@@ -63,20 +63,16 @@ write_close.addEventListener("click", function(evt) {
 window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === escKeyCode) {
         evt.preventDefault();
-        if (write_popap.classList.contains("modal-show")) {
-            write_popap.classList.remove("modal-show");
+        if (write_popap.classList.contains("modal--show")) {
+            write_popap.classList.remove("modal--show");
             write_popap.classList.remove("modal-error");
-            overlay.classList.remove('active');
-        }
-        if (map_popap.classList.contains("modal-show")) {
-            map_popap.classList.remove("modal-show");
             overlay.classList.remove('active');
         }
     }
 });
 
 overlay.addEventListener('click', function() {
-    document.querySelector('.modal.modal-show').classList.remove('modal-show');
+    document.querySelector('.modal.modal--show').classList.remove('modal--show');
     this.classList.remove('active');
 });
 
